15e478ab0d2b62a358b27d45b7fe8e146da78e2b
Aleksa Avramovic <some12curious@gmail.com>
2019-06-16 14:33:23 +0200

    Fixed imports, added methods for getting, popping and inserting tasks.

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import QObject
-from ..tasks.objects import TaskObject, create_task_object
-from ..storage import Storage
+from tasks.objects import TaskObject, create_task_object
+from storage import Storage
 
 
 class TasksModel(QObject):
@@ -8,7 +8,20 @@ class TasksModel(QObject):
     def __init__(self, todo_name):
         self.name = todo_name
         self._storage = Storage()
+        self._tasks = None
         self._load_tasks()
 
     def _load_tasks(self):
         self._tasks = [TaskObject(raw_task) for raw_task in self._storage.tasks(self.name)]
+
+    def tasks(self):
+        return self._tasks
+
+    def get_task(self, index):
+        return self._tasks[index]
+
+    def insert_task(self, index, task_object):
+        return self._tasks.insert(index, task_object)
+
+    def pop_task(self, index):
+        return self._tasks.pop(index)
