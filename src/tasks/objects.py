cb746f17fdef12c03ecf7dd2d4d434006af91b6a
Aleksa Avramovic <some12curious@gmail.com>
2019-06-25 18:37:33 +0200

    Refactor TaskObject creation.

--- src/tasks/objects.py
+++ src/tasks/objects.py
@@ -2,22 +2,25 @@ import datetime
 
 
 def create_task_object(description):
-    json_object = {
-        'description': description,
-        'date': datetime.datetime.now().timestamp()
-    }
-    task_object = TaskObject(json_object)
+    date = datetime.datetime.now().timestamp()
+    task_object = TaskObject()
+    task_object.description = description
+    task_object.date = date
     return task_object
 
 
 class TaskObject(object):
 
-    def __init__(self, json_object):
-        if not isinstance(json_object, dict):
-            raise TypeError('Expected dict type, got {} instead'.format(json_object))
-
-        self.description = json_object['description']
-        self.date = json_object['date']
+    def __init__(self, json_object=None):
+        if json_object is None:
+            self.description = None
+            self.date = None
+            return
+        elif isinstance(json_object, dict):
+            self.description = json_object['description']
+            self.date = json_object['date']
+        else:
+            raise TypeError('Expected dict or None, got {} instead'.format(json_object))
 
     def to_json(self):
         return {'description': self.description, 'date': self.date}
