45127892465b9f19b8698951f9ed75386eaf7d76
Aleksa Avramovic <some12curious@gmail.com>
2019-06-29 13:40:10 +0200

    Added get method and fixed update rules method.

--- src/cards/rules.py
+++ src/cards/rules.py
@@ -4,12 +4,19 @@ class Rules(object):
 
     def __init__(self, card_name):
         self._storage = Storage()
+        self.card_name = card_name
         self.rules = self._storage.rules(card_name)
 
-    def update_rules(self, new_rules):
+    def update(self, new_rules):
         # add_rules is actually faster than update_rules
         # because it doesn't have to check if rules exist.
-        self._storage.add_rules(new_rules)
+        self._storage.add_rules(self.card_name, new_rules)
+        self.rules = new_rules
+
+    def get(self, key, default):
+        if self.rules:
+            return self.rules[key]
+        return default
 
     def __getitem__(self, item):
         return self.rules[item]
