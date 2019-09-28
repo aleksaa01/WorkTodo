a1aa377dd73e085abfd786158dc60592eb01c856
Aleksa Avramovic <some12curious@gmail.com>
2019-09-28 06:28:23 +0200

    Fixed some vague variable names, and saving method.

--- src/storage.py
+++ src/storage.py
@@ -1,7 +1,8 @@
 import os
 import json
 from utils.singletons import GenericSingleton
-from api.resources import Dispatcher
+from api.dispatcher import ApiCallDispatcher
+from api.resources import CardResource, TaskResource, PreferenceResource
 from queue import Queue
 from PyQt5.QtWidgets import QApplication
 
@@ -21,12 +22,12 @@ class Storage(object, metaclass=GenericSingleton):
         else:
             self.path = os.path.join(os.getcwd(), self.name)
 
+        self.cards = None
+        self.tasks = None
+        self.preferences = None
+        self.token = None
         with open(self.path, 'r') as f:
-            storage = json.load(f)
-
-        self._cards = storage["cards"]
-        self._preferences = storage["preferences"]
-        self._token = storage['token']
+            self.load_from_file(f)
 
         # Check saved attribute to see if file content and storage object are synchronized.
         self.saved = True
@@ -34,31 +35,38 @@ class Storage(object, metaclass=GenericSingleton):
         self.debug = False
 
         self.output_queue = Queue()
-        self.disp = Dispatcher(self.output_queue)
-        if self._token:
-            self.disp.token = token
+        self.dispatcher = ApiCallDispatcher(self.output_queue)
+        if self.token:
+            self.dispatcher.token = token
+
+    def load_from_file(self, f):
+        data = json.load(f)
+        self.cards = [CardResource.from_json(resource) for resource in data['cards']]
+        self.tasks = [TaskResource.from_json(resource) for resource in data['tasks']]
+        self.preferences = [PreferenceResource.from_json(resource) for resource in data['preferences']]
+        self.token = data['token']
 
     def is_authenticated(self):
-        if self._token is None:
+        if self.token is None:
             return False
-        assert isinstance(self._token, str), 'Token should be represented by string, got {} instead'.format(type(self._token))
+        assert isinstance(self.token, str), 'Token should be represented by string, got {} instead'.format(type(self.token))
         return True
 
     def authenticate(self, username, password):
-        self.disp.authenticate(1, username, password)
+        self.dispatcher.authenticate(0, username, password)
 
         while True:
             QApplication.processEvents()
             if self.output_queue.empty():
                 continue
             job_id, future = self.output_queue.get()
-            if job_id != 1:
+            if job_id != 0:
                 self.output_queue.put((job_id, future))
                 continue
 
             token = future.result()
-            self.disp.token = token
-            self._token = token
+            self.dispatcher.token = token
+            self.token = token
             break
         return True
 
@@ -122,7 +130,10 @@ class Storage(object, metaclass=GenericSingleton):
         if self.debug:
             return
         with open(self.path, 'w') as f:
-            storage = {"cards": self._cards, "preferences": self._preferences}
-            json.dump(storage, f)
+            cards_resource = [card.to_json() for card in self.cards]
+            tasks_resource = [task.to_json() for task in self.tasks]
+            preferences_resource = [pref.to_json() for pref in self.preferences]
+            data = {'cards': cards_resource, 'tasks': tasks_resource, 'preferences': preferences_resource}
+            json.dump(data, f)
         self.saved = True
         print("Storage state saved!")
