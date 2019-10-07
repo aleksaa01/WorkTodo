a2f7a1cb4e6d1e548f996336762b4d41052a5bff
Aleksa Avramovic <some12curious@gmail.com>
2019-10-08 00:14:22 +0200

    Removed old model for cards.

--- src/cards/models.py
+++ src/cards/models.py
@@ -3,24 +3,6 @@ from api.resources import CardResource
 from tasks.models import TaskModel
 
 
-class CardModel(object):
-
-    def __init__(self):
-        self._storage = Storage()
-        self._cards = self._storage.cards()
-
-    def cards(self):
-        return self._cards
-
-    def add_card(self, card_name):
-        self._cards.append(card_name)
-        self._storage.add_card(card_name)
-
-    def remove_card(self, card_name):
-        self._cards.remove(card_name)
-        self._storage.remove_card(card_name)
-
-
 class CardsModel(object):
 
     def __init__(self, storage):
