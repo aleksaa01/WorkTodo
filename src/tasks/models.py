aa56a54d87aded012799b56f281ce71fbed3d7c0
aleksaa01 <some12curious@gmail.com>
2019-12-26 12:35:07 +0100

    Fix few TasksModel methods. Add "find", "insert", "__len__".

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -3,23 +3,27 @@ from api.resources import TaskResource
 from storage import Storage
 
 
-class TasksModel(QObject):
+class TasksModel(object):
 
     def __init__(self, storage, card_rid):
         self._st = storage
         self.crid = card_rid
         self._last_rid = None
 
-    def data(self):
-        return [(task.rid, task.description, task.created)
-                for task in self._st.tasks[self.crid]]
+    def data(self, index=None):
+        if index is None:
+            return [(task.rid, task.description, task.created)
+                    for task in self._st.tasks(self.crid)]
+        task = self._st.get_task(self.crid, index)
+        return (task.rid, task.description, task.created)
 
-    def add(self, text):
+    def add(self, text, created):
         # Should I set position of the task here ? Seems unnecessary though.
-        task = TaskResource(description=text)
         new_rid = self._get_new_rid()
-        task.rid = new_rid
+        task = TaskResource(rid=new_rid, description=text,
+                            card_rid=self.crid, created=created)
         self._st.add_task(self.crid, task)
+        return new_rid
 
     def remove(self, task_index):
         self._st.pop_task(self.crid, task_index)
@@ -27,10 +31,26 @@ class TasksModel(QObject):
     def move(self, old_idx, new_idx):
         self._st.move_task(self.crid, old_idx, new_idx)
 
-    def update(self, task_index, text):
-        task = self._st.get_task(task_index)
+    def update(self, task_index, text, created_at=None):
+        task = self._st.get_task(self.crid, task_index)
         task.description = text
-        self._st.update_task(task)
+        if created_at:
+            task.created = created_at
+        self._st.update_task(self.crid, task_index, task)
+
+    def find(self, task_rid):
+        for idx, task in enumerate(self._st.tasks(self.crid)):
+            if task.rid == task_rid:
+                return idx
+        return -1
+
+    def insert(self, index, text, created):
+        new_rid = self._get_new_rid()
+        task = TaskResource(rid=new_rid, description=text,
+                            card_rid=self.crid, created=created)
+        self._st.insert_task(self.crid, index, task)
+        return new_rid
+
 
     def _get_new_rid(self):
         new_rid = self._last_rid + 1 if self._last_rid else 0
@@ -43,3 +63,6 @@ class TasksModel(QObject):
     def __getitem__(self, idx):
         task = self._st.get_task(self.crid, idx)
         return (task.rid, taks.description, task.created)
+
+    def __len__(self):
+        return len(self._st.tasks(self.crid))
