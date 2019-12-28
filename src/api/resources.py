5d49c08430ce3dcf951231f607d720b57e355f5f
aleksaa01 <some12curious@gmail.com>
2019-12-28 15:53:20 +0100

    Introduce field_map(which gives you an ability to have differently named fields than the api ones).

--- src/api/resources.py
+++ src/api/resources.py
@@ -2,13 +2,28 @@ from datetime import datetime
 
 
 class ResourceBase(object):
+    """
+    #field_map: Use it if you want to name resource fields differently
+        from how are they named in the api.
+    """
+    field_map = None
 
     @classmethod
     def from_json(cls, json_resource):
         return cls(**json_resource)
 
     def to_json(self):
-        return vars(self)
+        if self.field_map is None:
+            return vars(self)
+
+        output = {}
+        for field_name, val in vars(self):
+            if field_name in self.field_map:
+                new_field_name = self.field_map[field_name]
+                output[new_field_name] = val
+            else:
+                output[field_name] = val
+        return output
 
 
 class CardResource(ResourceBase):
@@ -18,8 +33,6 @@ class CardResource(ResourceBase):
         self.name = kwargs['name']
         # For perfermance reasons, you can update position just before API synchronization
         self.position = kwargs['position']
-        self.tasks = kwargs.get('tasks', [])
-        self.preferences = kwargs.get('preferenecs', [])
 
 
 class TaskResource(ResourceBase):
